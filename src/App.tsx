4189c2c10a8b160fcf3c175c9ffeaadaad1af091
taiki-nd <kii.develop.0517@gmail.com>
2022-10-10 21:02:53 +0900

    ボトムタブの生成

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,19 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { HomeScreen } from './screens/HomeScreens';
 import { UserShow } from './screens/users/UserShow';
 
-const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen
-          name="UserShow"
-          component={UserShow}
-          options={(props) => ({
-            title: `ユーザー詳細画面${props.route.params.userId}`,
-          })} />
-      </Stack.Navigator>
+      <Tab.Navigator initialRouteName="Home">
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="User" component={UserShow} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
