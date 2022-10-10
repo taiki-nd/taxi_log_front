5c10daf0bcc2a73dae8c52236293c1f257b049fa
taiki-nd <kii.develop.0517@gmail.com>
2022-10-10 20:43:48 +0900

    react nativeにおけるuseEffect

--- src/screens/HomeScreens.tsx
+++ src/screens/HomeScreens.tsx
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View, Text, Button } from 'react-native';
 
 export const HomeScreen = (props: any) => {
+  useEffect(() => {
+    console.log('Home Mount');
+    return () => {
+      console.log('Home Unmount');
+    };
+  }, []);
   return (
     <View>
       <Text>ホーム画面</Text>
       <Button
-        title="ユーザ"
+        title="ユーザー"
         onPress={() => props.navigation.navigate('UserShow', {userId: 1})}/>
     </View>
   );
--- src/screens/users/UserShow.tsx
+++ src/screens/users/UserShow.tsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View, Text } from 'react-native';
 
 export const UserShow = (props: any) => {
   console.log("route", props.route);
+  useEffect(() => {
+    console.log('Home Mount');
+    return () => {
+      console.log('Home Unmount');
+    };
+  }, []);
   return (
     <View>
       <Text>ユーザー画面</Text>
