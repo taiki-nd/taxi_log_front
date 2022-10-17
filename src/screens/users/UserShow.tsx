47e8ba07e22592ec83b871920ab34292c0242624
taiki-nd <kii.develop.0517@gmail.com>
2022-10-17 21:13:09 +0900

    iconの導入

--- src/screens/users/UserShow.tsx
+++ src/screens/users/UserShow.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { View, Text } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 export const UserShow = (props: any) => {
   console.log("route", props.route);
@@ -12,6 +13,7 @@ export const UserShow = (props: any) => {
   return (
     <View>
       <Text>ユーザー画面</Text>
+      <Icon name="home" size={50}/>
     </View>
   );
 };
