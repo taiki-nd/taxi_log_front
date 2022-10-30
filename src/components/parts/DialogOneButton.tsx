1929149fcc4a083bcad73a52662a338145020684
taiki-nd <kii.develop.0517@gmail.com>
2022-10-31 00:54:08 +0900

    ダイアログの修正

--- src/components/parts/DialogOneButton.tsx
+++ src/components/parts/DialogOneButton.tsx
@@ -1,5 +1,6 @@
-import { View } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import { Provider, Portal, Dialog, Paragraph, Button } from 'react-native-paper';
+import { BackColor, BasicColor, CoverColor } from '../../styles/common/color';
 
 export const DialogOneButton = (props: any) => {
   const {title, description, displayButton1, visible, funcButton1, onDismiss } = props;
@@ -8,12 +9,12 @@ export const DialogOneButton = (props: any) => {
         <View>
           <Portal>
             <Dialog visible={visible} onDismiss={onDismiss}>
-              <Dialog.Title>{title}</Dialog.Title>
+              <Dialog.Title style={styles.text}>{title}</Dialog.Title>
               <Dialog.Content>
-                <Paragraph>{description}</Paragraph>
+                <Paragraph style={styles.text}>{description}</Paragraph>
               </Dialog.Content>
               <Dialog.Actions>
-                <Button onPress={funcButton1}>{displayButton1}</Button>
+                <Button onPress={funcButton1} textColor={BackColor}>{displayButton1}</Button>
               </Dialog.Actions>
             </Dialog>
           </Portal>
@@ -21,3 +22,12 @@ export const DialogOneButton = (props: any) => {
       </Provider>
   );
 }
+
+const styles = StyleSheet.create({
+  text :{
+    color: BasicColor,
+  },
+  dialog: {
+    backgroundColor: CoverColor,
+  }
+});
--- src/components/parts/DialogTwoButton.tsx
+++ src/components/parts/DialogTwoButton.tsx
@@ -1,5 +1,6 @@
-import { View } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import { Provider, Portal, Dialog, Paragraph, Button } from 'react-native-paper';
+import { BasicColor, CoverColor, SeaColor, TomatoColor } from '../../styles/common/color';
 
 export const DialogTwoButton = (props: any) => {
   const {title, description, displayButton1, displayButton2, visible, funcButton1, funcButton2, onDismiss } = props;
@@ -7,14 +8,14 @@ export const DialogTwoButton = (props: any) => {
     <Provider>
         <View>
           <Portal>
-            <Dialog visible={visible} onDismiss={onDismiss}>
-              <Dialog.Title>{title}</Dialog.Title>
+            <Dialog visible={visible} onDismiss={onDismiss} style={styles.dialog}>
+              <Dialog.Title style={styles.text}>{title}</Dialog.Title>
               <Dialog.Content>
-                <Paragraph>{description}</Paragraph>
+                <Paragraph style={styles.text}>{description}</Paragraph>
               </Dialog.Content>
               <Dialog.Actions>
-                <Button onPress={funcButton1}>{displayButton1}</Button>
-                <Button onPress={funcButton2}>{displayButton2}</Button>
+                <Button onPress={funcButton1} textColor={SeaColor}>{displayButton1}</Button>
+                <Button onPress={funcButton2} textColor={TomatoColor}>{displayButton2}</Button>
               </Dialog.Actions>
             </Dialog>
           </Portal>
@@ -22,3 +23,12 @@ export const DialogTwoButton = (props: any) => {
       </Provider>
   );
 }
+
+const styles = StyleSheet.create({
+  text :{
+    color: BasicColor,
+  },
+  dialog: {
+    backgroundColor: CoverColor,
+  }
+});
