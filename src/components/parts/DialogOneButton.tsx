6ebf48cdf767e405e06dbc8b745ccb8defb6507b
taiki-nd <kii.develop.0517@gmail.com>
2022-11-01 20:05:47 +0900

    修正

--- src/components/parts/DialogOneButton.tsx
+++ src/components/parts/DialogOneButton.tsx
@@ -1,6 +1,6 @@
 import { View, StyleSheet } from 'react-native';
 import { Provider, Portal, Dialog, Paragraph, Button } from 'react-native-paper';
-import { BackColor, BasicColor, CoverColor } from '../../styles/common/color';
+import { BasicColor, CoverColor, SeaColor } from '../../styles/common/color';
 
 export const DialogOneButton = (props: any) => {
   const {title, description, displayButton1, visible, funcButton1, onDismiss } = props;
@@ -14,7 +14,7 @@ export const DialogOneButton = (props: any) => {
                 <Paragraph style={styles.text}>{description}</Paragraph>
               </Dialog.Content>
               <Dialog.Actions>
-                <Button onPress={funcButton1} textColor={BackColor}>{displayButton1}</Button>
+                <Button onPress={funcButton1} textColor={SeaColor}>{displayButton1}</Button>
               </Dialog.Actions>
             </Dialog>
           </Portal>
