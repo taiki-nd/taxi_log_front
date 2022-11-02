d36eb47266e0b386cea9a79360e35fbf95d85d40
taiki-nd <kii.develop.0517@gmail.com>
2022-11-02 21:27:19 +0900

    viewの作成

--- src/screens/RecordsCreate.tsx
+++ src/screens/RecordsCreate.tsx
@@ -1,24 +1,106 @@
-import React, { useEffect } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import { BackColor } from '../styles/common/color';
+import React, { SyntheticEvent, useCallback, useEffect, useState } from 'react';
+import { Text, StyleSheet, View, TouchableWithoutFeedback, Keyboard } from 'react-native';
+import { RadioButton } from 'react-native-paper';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
+import { StandardTextInput } from '../components/parts/StandardTextInput';
+import { StandardLabel } from '../components/parts/StandardLabel';
+import { StandardButton } from '../components/parts/StandardButton';
+import { StandardTextLink } from '../components/parts/StandardTextLink';
+import { AccentColor, BackColor } from '../styles/common/color';
 
 export const RecordsCreate = (props: any) => {
-  useEffect(() => {
-    console.log('Home Mount');
-    return () => {
-      console.log('Home Unmount');
-    };
-  }, []);
+  // props
+  const { navigation } = props;
+
+  // state
+  const [date, setDate] = useState('');
+  const [day, setDay] = useState('');
+  const [styleFlg, setStyleFlg] = useState('');
+  const [startHour, setStartHour] = useState(Number);
+  const [runningTime, setRunningTime] = useState(Number);
+  const [occupancyRate, setOccupancyRate] = useState(Number);
+  const [numberOfTime, setNumberOfTime] = useState(Number);
+  const [isTax, setIsTax] = useState(Boolean);
+  const [dailySales, setDailySales] = useState(Number);
+  const [buttonDisabled, setButtonDisabled] = useState(true);
+
+  const createRecord = () => {
+    console.log("pressed createRecord button");
+  }
+
+  /**
+   * moveScreen
+   * @param screen 
+   */
+  const moveScreen = (screen: any) => {
+    navigation.reset({
+      index: 0,
+      routes: [{ name: screen }]
+    });
+  }
+
   return (
-    <View style={styles.background}>
-      <Text>レコード作成</Text>
+    <View style={styles.mainBody}>
+      <View>
+        <KeyboardAwareScrollView>
+          <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+            <View>
+              <StandardTextInput label="日付" placeholder="------" keyboardType="default" secureTextEntry={false} onChangeText={(text: string) => setDate(text)}/>
+              <StandardLabel displayText={"曜日"}/>
+              <RadioButton.Group onValueChange={value => setDay(value)} value={day}>
+                <RadioButton.Item label="月" value="every_other_day" style={styles.radioButtonStyle} color={AccentColor}/>
+                <RadioButton.Item label="火" value="day" style={styles.radioButtonStyle} color={AccentColor}/>
+                <RadioButton.Item label="水" value="night" style={styles.radioButtonStyle} color={AccentColor}/>
+                <RadioButton.Item label="木" value="other" style={styles.radioButtonStyle} color={AccentColor}/>
+                <RadioButton.Item label="金" value="other" style={styles.radioButtonStyle} color={AccentColor}/>
+                <RadioButton.Item label="土" value="other" style={styles.radioButtonStyle} color={AccentColor}/>
+                <RadioButton.Item label="日" value="other" style={styles.radioButtonStyle} color={AccentColor}/>
+              </RadioButton.Group>
+              <StandardLabel displayText={"勤務形態"}/>
+              <RadioButton.Group onValueChange={value => setStyleFlg(value)} value={styleFlg}>
+                <RadioButton.Item label="隔日勤務" value="every_other_day" style={styles.radioButtonStyle} color={AccentColor}/>
+                <RadioButton.Item label="日勤" value="day" style={styles.radioButtonStyle} color={AccentColor}/>
+                <RadioButton.Item label="夜勤" value="night" style={styles.radioButtonStyle} color={AccentColor}/>
+                <RadioButton.Item label="他" value="other" style={styles.radioButtonStyle} color={AccentColor}/>
+              </RadioButton.Group>
+              <StandardTextInput label="始業開始時刻" placeholder="15" keyboardType="default" secureTextEntry={false} onChangeText={(i: number) => setStartHour(i)}/>
+              <StandardTextInput label="走行時間" placeholder="17" keyboardType="default" secureTextEntry={false} onChangeText={(i: number) => setRunningTime(i)}/>
+              <StandardTextInput label="乗車率" placeholder="55" keyboardType="default" secureTextEntry={false} onChangeText={(i: number) => setOccupancyRate(i)}/>
+              <StandardTextInput label="乗車回数" placeholder="38" keyboardType="default" secureTextEntry={false} onChangeText={(i: number) => setNumberOfTime(i)}/>
+              <StandardTextInput label="売上" placeholder="58000" keyboardType="default" secureTextEntry={false} onChangeText={(i: number) => setNumberOfTime(i)}/>
+              
+              {/*errorMessages.length != 0 ? (
+                errorMessages.map((errorMessage: string, index: number) => { 
+                  return(
+                    <Text style={styles.errorTextStyle} key={index}>
+                      {errorMessage}
+                    </Text>
+                  )})
+                  ) : null*/}
+              <StandardButton displayText="Create Record" disabled={buttonDisabled} onPress={createRecord}/>
+              <StandardTextLink displayText="Cancel" onPress={() => moveScreen("Home")}/>
+            </View>
+          </TouchableWithoutFeedback>
+        </KeyboardAwareScrollView>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  background: {
+  mainBody: {
     flex: 1,
-    backgroundColor: BackColor
-  }
+    justifyContent: 'center',
+    backgroundColor: BackColor,
+    alignContent: 'center',
+  },
+  radioButtonStyle: {
+    marginLeft: 35,
+    marginRight: 35,
+  },
+  errorTextStyle: {
+    color: 'red',
+    textAlign: 'center',
+    fontSize: 14,
+  },
 });
