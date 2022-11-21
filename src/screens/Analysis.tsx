ef4b49dd951cf3ca1d3cceb8062203c5fd1450b5
taiki-nd <kii.develop.0517@gmail.com>
2022-11-22 00:03:15 +0900

    グラフの表示

--- src/screens/Analysis.tsx
+++ src/screens/Analysis.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import { BackColor } from '../styles/common/color';
+import { View, StyleSheet, Dimensions } from 'react-native';
+import { Text } from "react-native-paper";
+import { LineChart } from 'react-native-chart-kit';
+import { AccentColor, BackColor, BasicColor } from '../styles/common/color';
 
 export const Analysis = (props: any) => {
   console.log("route", props.route);
@@ -11,15 +13,58 @@ export const Analysis = (props: any) => {
     };
   }, []);
   return (
-    <View style={styles.background}>
-      <Text>分析</Text>
+    <View style={styles.mainBody}>
+      <View>
+        <Text variant="titleMedium" style={styles.subTitle}>月次総売上</Text>
+        <LineChart
+          data={{
+              labels: ['1月', '2月', '3月', '4月', '5月', '6月'],
+              datasets: [{
+                  data: [
+                      Math.random() * 100,
+                      Math.random() * 100,
+                      Math.random() * 100,
+                      Math.random() * 100,
+                      Math.random() * 100,
+                      Math.random() * 100
+                  ]
+              }]
+          }}
+          width={Dimensions.get("window").width} // from react-native
+          height={220}
+          yAxisLabel='¥'
+          chartConfig={{
+              backgroundColor: BackColor,
+              backgroundGradientFrom: BackColor,
+              backgroundGradientTo: BackColor,
+              color: (opacity = 0.5) => `rgba(63, 62, 52, ${opacity})`,
+              propsForDots: {
+                r: "2",
+                strokeWidth: "2",
+                stroke: BasicColor
+              }
+          }}
+          style={{
+            marginVertical: 8,
+            borderRadius: 16
+          }}
+        />
+      </View>
+      
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  background: {
+  mainBody: {
+    padding: 10,
     flex: 1,
-    backgroundColor: BackColor
-  }
+    backgroundColor: BackColor,
+    alignContent: 'center',
+  },
+  subTitle: {
+    color: BasicColor,
+    fontWeight: 'bold',
+    textDecorationLine: 'underline',
+  },
 });
