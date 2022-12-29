1cb35d608c40fb73d871d2b88e1ecfe415dffaa3
taiki-nd <kii.develop.0517@gmail.com>
2022-12-29 09:45:56 +0900

    home画面の再実装

--- src/utils/commonFunc/user/GetSigninUser.tsx
+++ src/utils/commonFunc/user/GetSigninUser.tsx
@@ -1,10 +1,20 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import axios from "axios";
+import { auth } from "../../../auth/firebase";
 import { User } from "../../../models/User";
 import { method } from "../../const";
 
-export const GetSigninUser = async (uid: string) => {
+export const GetSigninUser = async () => {
   console.log("start get signin user----------------------------------------------------------------")
-  // headers
+  
+  // uid取得
+  var currentUser = auth.currentUser
+  var uid: string
+  if (currentUser) {
+    uid = currentUser.uid
+  } else {
+    return false;
+  }
   const headers = {'uuid': uid}
   await axios({
     method: method.GET,
@@ -15,9 +25,13 @@ export const GetSigninUser = async (uid: string) => {
   }).then((response) => {
     var user: User = response.data.data
     console.log("user", user);
-    return user;
+    AsyncStorage.setItem("taxi_log_user_id", String(user.id));
+    return true;
   }).catch(error => {
     console.log("error", error);
-    return "error";
+    // user情報取得失敗
+    return false;
   });
+  
+  return true;
 }
--- src/utils/commonFunc/user/Signout.tsx
+++ src/utils/commonFunc/user/Signout.tsx
@@ -1,10 +1,12 @@
 import { signOut } from '@firebase/auth';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { auth } from '../../../auth/firebase';
 
 export const FuncSignout = (props: any) => {
   const { navigation } = props;
   signOut(auth).then(() => {
     console.log('signout')
+    AsyncStorage.removeItem('taxi_log_user_id')
     navigation.reset({
       index: 0,
       routes: [{ name: 'Signin' }]
